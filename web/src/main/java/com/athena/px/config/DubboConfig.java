8683c31e55ee0b4c3c69b4b98859ad8a4d57f054
sl520311 <598968402@qq.com>
2018-05-24 16:54:18 +0800

    'add_dubbo_config'

---
+++ web/src/main/java/com/athena/px/config/DubboConfig.java
@@ -0,0 +1,16 @@
+package com.athena.px.config;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @Description:
+ * @Author: ShenLong
+ * @CreateDate: 2018/5/24 16:26
+ */
+@Configuration
+@EnableDubboConfig
+@PropertySource("dubbo-config.properties")
+public class DubboConfig {
+}
